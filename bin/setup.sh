0f6cf15f2d9acf174c2a872b1f6089803c2aba9f
amiracle <splunk@sh01.gridgig.com>
2018-05-07 20:29:04 +0000

    Added features to setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,35 +3,44 @@
 # This script will help setup your matketplace.sh file
 
 # Set your local directories
-directory=$SPLUNK_HOME/etc/apps/aws_marketplace_app_for_splunk/bin
 awscli=$HOME/.aws
-script=$directory/marketplace.sh
-awsvars=awsvars
+awsvar=awsvars
+awscommand=`which aws`
 
-# Checks for the marketplace.sh script
-if [-f "$script" ];then
-echo "Script exists, no need to run this setup script."
-else 
-echo "Let's create your marketplace shell script."
-echo "First, let's populate the variables file."
-	echo "Creating your marketplace.sh script."
-	echo "Type in your AWS Account ID"
-	read account_id
-	echo accountid="$account_id" > awsvars
-	echo "Name of your S3 Bucket"
-	read s3bucket
-	echo  s3bucket="$s3bucket" >> awsvars
-	echo "Role ARN"
-	read rolearn
-	echo  rolearn="$rolearn" >> awsvars
-	echo "SNS ARN"
-	read snsarn
-	echo snsarn="$snsarn" >> awsvars
-	echo "Created aws variables file."
+# Checks for the aws CLI toolkit
+
+if [ "$awscommand" != "no"  ];then
+echo "AWS CLI does NOT exist, please read https://docs.aws.amazon.com/cli/latest/userguide/awscli-install-bundle.html"
+else
+echo "AWS CLI exists."
 fi
+
 # Checks for aws cli and directory (.aws) in home directory
+
 if [ -d "$awscli" ];then
 echo "AWS CLI and config directory exist."
 else
 echo "Please run aws configure to create .aws directory. If you do not have the AWS CLI, please install."
+fi
+
+# Checks for AWS Variables file 
+
+if [ -f "$awsvar" ];then
+echo "Variable file exists."
+else
+	echo "Let's populate the variables file."
+        echo "Creating your marketplace.sh script."
+        echo "Type in your AWS Account ID"
+        read account_id
+        echo accountid="$account_id" > awsvars
+        echo "Name of your S3 Bucket"
+        read s3bucket
+        echo  s3bucket="$s3bucket" >> awsvars
+        echo "Role ARN"
+        read rolearn
+        echo  rolearn="$rolearn" >> awsvars
+        echo "SNS ARN"
+        read snsarn
+        echo snsarn="$snsarn" >> awsvars
+        echo "Created aws variables file."
 fi
