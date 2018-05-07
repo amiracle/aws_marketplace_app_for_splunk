d22d864da42c7d15195ebd523aaf33e505eaf712
Kamilo Amir <kamilo@gmail.com>
2018-05-07 11:58:43 -0400

    Update marketplace.sh

--- bin/marketplace.sh
+++ bin/marketplace.sh
@@ -6,6 +6,6 @@ ad="`date +%Y-%m-%d`"
 #echo "$ad"T00:00:00Z
 while read -r billing; do
         echo "Generate Billing $billing..."
-        aws marketplacecommerceanalytics generate-data-set --data-set-type "$billing" --data-set-publication-date ""$ad"T00:00:00Z" --role-name "arn:aws:iam::841176192222:role/MarketplaceCommerceAnalyticsRole" --destination-s3-bucket-name "splunk-marketplaceami" --sns-topic-arn "arn:aws:sns:us-east-1:841176192222:splunk-marketplace-commerce"
+        aws marketplacecommerceanalytics generate-data-set --data-set-type "$billing" --data-set-publication-date ""$ad"T00:00:00Z" --role-name "arn:aws:iam::accountid:role/MarketplaceCommerceAnalyticsRole" --destination-s3-bucket-name "s3bucket" --sns-topic-arn "arn:aws:sns:region:accountid:my-marketplace-commerce"
 
 done < billing
